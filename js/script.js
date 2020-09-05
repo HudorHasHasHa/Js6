2caf3813936b0f2ccc7f7bea3a98780c4222148c
Igor Jankowski <jankowskiigor5@gmail.com>
2020-09-05 11:15:15 +0200

    Commit6.3.1

--- js/script.js
+++ js/script.js
@@ -10,14 +10,18 @@ document.getElementById('test-button').addEventListener('click', function(){
  const titleClickHandler = function(event){
   console.log('Link was clicked!');
   console.log(event);
-}
+
 
 // Remove class 'active' from all article links //
+
+
+
 /* add class 'active' to the clicked link */
 /* remove class 'active' from all articles */
 /* get href attribute from clicked link */
 /* find the correct article using the selector (value of href attribute) */
 /* add class 'active' to the correct article */
+ }
 
 const links = document.querySelectorAll('.titles a');
 
