bfec4be8777b088e0a7087fec11130f83805dd66
Igor Jankowski <jankowskiigor5@gmail.comgit congig --global -lgit config --global user.email jankowskiigor5@gmail.com>
2020-09-06 22:31:05 +0200

    ESLint_Conf_mod7.1

--- js/script.js
+++ js/script.js
@@ -13,31 +13,31 @@ const titleClickHandler = function(event){
   console.log('Link was clicked!');
   console.log(event);
 
-// [DONE] Remove class 'active' from all article links //
+  /* [DONE] Remove class 'active' from all article links */
   const activeLinks = document.querySelectorAll('.titles a.active');
 
   for(let activeLink of activeLinks){
-  activeLink.classList.remove('active');
-}
-/* add class 'active' to the clicked link */
+    activeLink.classList.remove('active');
+  }
+  /* add class 'active' to the clicked link */
   console.log('clickedElement:', clickedElement);
   clickedElement.classList.add('active');
 
-/* [DONE] remove class 'active' from all articles */
+  /* [DONE] remove class 'active' from all articles */
   const activeArticles = document.querySelectorAll('.posts article.active');
 
   for(let activeArticle of activeArticles){
     activeArticle.classList.remove('active');
   } 
-/* get href attribute from clicked link */
+  /* get href attribute from clicked link */
   const hrefvalue = clickedElement.getAttribute('href');
   console.log(hrefvalue);
-/* find the correct article using the selector (value of href attribute) */
+  /* find the correct article using the selector (value of href attribute) */
   const targetArticle = document.querySelector(hrefvalue);
   console.log(targetArticle);
-/* add class 'active' to the correct article */
+  /* add class 'active' to the correct article */
   targetArticle.classList.add('active');
-}
+};
 
 const optArticleSelector = '.post',
   optTitleSelector = '.post-title',
@@ -80,7 +80,7 @@ function generateTitleLinks(){
   for(let link of links){
     link.addEventListener('click', titleClickHandler);
   }
-    console.log(links);
+  console.log(links);
 }
 
 generateTitleLinks();
